3fcc695d969bd080a801e5d5bd8ece2b7dcc0902
paramgupta33 <paramgupta330@gmail.com>
2026-04-14 14:22:47 +0530

    fix: updated firebase config to new project

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,18 +1,17 @@
-import { initializeApp } from "firebase/app"
-import { getAuth } from "firebase/auth"
-import { getFirestore } from "firebase/firestore"
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore"; // ADD THIS
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
-}
-console.log("PROJECT ID:", import.meta.env.VITE_FIREBASE_PROJECT_ID)
-console.log("FIREBASE CONFIG:", firebaseConfig)
+};
 
-
-const app = initializeApp(firebaseConfig)
-
-export const auth = getAuth(app)
-export const db = getFirestore(app)
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app); // ADD THIS
+export default app;
